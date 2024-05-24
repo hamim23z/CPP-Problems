2688875f69e06ab1a1615ec68010d5e926530758
Hamim <hamimc232@gmail.com>
2024-05-24 19:10:43 -0400

    Create helloworld.cpp

---
+++ CPP-PracticeProblemsV1/helloworld.cpp
@@ -0,0 +1,11 @@
+/*TODO: Write a program that prints out "Hello world"*/
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+    cout << "Hello world" << endl;
+}
